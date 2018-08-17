042154e0a9029b3e24ccb11ede9aabd24bca277b
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2018-08-17 13:42:54 -0400

    Add glomeruli data download from greenelab/rheum-plier-data (#31)
    * Add glom data download from data repo
    
    Ignore downloaded files
    
    * Update: rerun DELV with data downloaded from data repo
    
    * Update: rerun with data from data repo
    
    * Update: rerun figure notebook with data downloaded from data repo
    
    No changes in p-values on display items

--- 00-data_download.sh
+++ 00-data_download.sh
@@ -27,6 +27,9 @@ wget https://github.com/greenelab/rheum-plier-data/raw/4be547553f24fecac9e2f5c2b
 # isolated blood cell populations from autoimmune conditions
 wget https://github.com/greenelab/rheum-plier-data/raw/4be547553f24fecac9e2f5c2b469a17f9df253f0/isolated-cell-pop/processed/E-MTAB-2452_hugene11st_SCANfast.pcl
 
+# glomeruli data
+wget https://github.com/greenelab/rheum-plier-data/raw/55d86bb537f9e38c83fc3cca993cde48dc984411/glomeruli/ERCB_Glom_CustCDF19_forVCRC.txt
+
 # get sample (e.g., phenotype) data
 cd .. && mkdir sample_info && cd sample_info
 # sle-wb sample to dataset of origin data
@@ -37,3 +40,5 @@ wget https://github.com/greenelab/rheum-plier-data/raw/4be547553f24fecac9e2f5c2b
 wget https://github.com/greenelab/rheum-plier-data/raw/4be547553f24fecac9e2f5c2b469a17f9df253f0/sle-wb/arrayexpress/E-GEOD-39088/E-GEOD-39088.sdrf.txt
 wget https://github.com/greenelab/rheum-plier-data/raw/4be547553f24fecac9e2f5c2b469a17f9df253f0/sle-wb/arrayexpress/E-GEOD-78193/E-GEOD-78193.sdrf.txt
 wget https://github.com/greenelab/rheum-plier-data/raw/4be547553f24fecac9e2f5c2b469a17f9df253f0/NARES/NARES_demographic_data.tsv
+wget https://github.com/greenelab/rheum-plier-data/raw/55d86bb537f9e38c83fc3cca993cde48dc984411/glomeruli/ERCB_glom_diagnosis.tsv
+
